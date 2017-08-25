e22dae2b93a18b877594463ad100e3d25b8898ea
theMladyPan <stanislav@rubint.sk>
2017-08-25 20:58:51 +0200

    "dynamic allocation and git tests"

--- dynamic_allocation/dyn.cpp
+++ dynamic_allocation/dyn.cpp
@@ -13,28 +13,28 @@ void statAlMem(){
  	int i;
 }
 
-int main(int argc, char *argv[]){            
+int main(int argc, char *argv[]){
   using namespace std;
 
-	/*if(argc>1){
+	if(argc>1){
 		cout<<argv[1]<<endl;
 	}
 
 	int *premenna = new int;
-	cout << *premenna<<endl;
-	
+	*premenna = 10;
+	cout << *premenna << "\n" << premenna << endl;
+
 	delete premenna;
-	
+
 	cout << *premenna << endl;
-	            */
-	/*      
-	for(int i=0;i<1000000;i++){
+
+
+	for(int i=0;i<1000000000;i++){
 		statAlMem();
-	}  */    
-	
+	}
+
 	int *a = new int[100000000];
-	
-	
-	
+	std::cout << a << '\n';
+
 	return 0;
 }
